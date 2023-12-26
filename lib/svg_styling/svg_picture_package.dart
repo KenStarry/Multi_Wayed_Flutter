fabce9a22e75cb2ddfb2966373b8f520e9892cdb
KenStarry <starrycodes@gmail.com>
2023-12-26 14:00:12 +0300

    Added Undraw pancakes svg image

---
+++ lib/svg_styling/svg_picture_package.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SvgPicturePackage extends StatefulWidget {
+  const SvgPicturePackage({super.key});
+
+  @override
+  State<SvgPicturePackage> createState() => _SvgPicturePackageState();
+}
+
+class _SvgPicturePackageState extends State<SvgPicturePackage> {
+  @override
+  Widget build(BuildContext context) {
+    return Text("hello");
+  }
+}
