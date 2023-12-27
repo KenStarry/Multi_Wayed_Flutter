87b3e329cd3abde71ffdab8066774c54c88ae96c
KenStarry <starrycodes@gmail.com>
2023-12-27 15:41:57 +0300

    Flutter SVG Styling

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:multi_wayed_flutter/svg_styling/flutter_shape_maker.dart';
-import 'package:multi_wayed_flutter/svg_styling/svg_picture_package.dart';
 import 'package:multi_wayed_flutter/svg_styling/svg_shader_gradient.dart';
 
 void main() {
